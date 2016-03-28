27543c0b212d8462fa3f8c8bf106a566d3390b27
abhishek <abhishek.tripathi@tradeleaves.com>
2016-03-28 16:11:00 +0530

    images added in assets

--- server/datasources/catalogDS.js
+++ server/datasources/catalogDS.js
@@ -9,15 +9,15 @@ module.exports = dataSources
 dataSources.processServiceRequest = function(cb){
     var host = 'localhost';
     var port = '9898';
-    var service = '/api/catalogs';
-    var apiUrl = 'http://'+host+':'+port+service;
+  /*  var service = '/api/catalogs';*/
+    var apiUrl = 'http://'+host+':'+port;
     return cb.apply(this,[apiUrl]);
 };
 
 dataSources.getActiveCatalogByCountry = function(countryCode,headers,cb){
     return this.processServiceRequest(function(apiUrl,err){
         if(!err){
-            var url = apiUrl+'/active/'+countryCode;
+            var url = apiUrl+'/api/catalogs/active/'+countryCode;
             if(headers && headers !== null){
                 request.get({url:url,headers:headers,json:true},cb);
             }else{
